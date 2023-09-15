45a3686dad5048a62be523dabcb8af815b44e9b4
Aliquis22 <108261563+Aliquis22@users.noreply.github.com>
2023-09-15 20:57:09 +0700

    Куб и оси

--- main.py
+++ main.py
@@ -18,11 +18,32 @@ class SoftwareRender: #Шаблон нашего приложения
         self.camera  = Camera(self, [0.5, 1,-4])
         self.projection = Projection(self)
         self.object = Object3D(self)
+
+
+        #куб и грани
         self.object.translate([0.2, 0.4, 0.2])
-        self.object.rotate_y((math.pi / 6))
+        self.axes = Axes(self)
+        self.axes.translate([0.7,0.9,0.7])
+        self.world_axes = Axes(self)
+        self.world_axes.movement_flag = False
+        self.world_axes.scale(2.5)
+        self.world_axes.translate([0.0001,0.0001,0.0001])
+
+    def get_object_from_file(self, filename):
+        vertex, faces = [], []
+        with open(filename) as f:
+            for line in f:
+                if line.startswith('v '):
+                    vertex.append([float(i) for i in line.split()[1:]] + [1])
+                elif line.startswith('f'):
+                    faces_ = line.split()[1:]
+                    faces.append([int(face_.split('/')[0])-1 for face_ in faces_])
+                return Object3D(self, vertex, faces)
 
     def draw(self): # Метод для отрисовки
         self.screen.fill(pg.Color('darkslategray'))
+        self.world_axes.draw()
+        self.axes.draw()
         self.object.draw()
 
 
--- object_3d.py
+++ object_3d.py
@@ -8,25 +8,40 @@ class Object3D: #Класс для взимодействия с главной 
                                   (0,0,1,1),(0,1,1,1),(1,1,1,1),(1,0,1,1)]) # массив вершин, однородные координаты
 
         self.faces = np.array([(0,1,2,3),(4,5,6,7),(0,4,5,1),(2,3,7,6),(1,2,6,5),(0,3,7,4)]) # массив граней куба
+
+        self.font = pg.font.SysFont('Arial', 30,bold = True) # шрифт
+        self.color_faces= [(pg.Color('orange'), face) for face in self.faces] # цвет
+        self.movement_flag, self.draw_vertexes = True, True # Триггер для отображения
+        self.label = '' # текст
     def draw(self):
         self.screen_projection()
+        self.movement() # вращение куба
+
+    def movement(self):
+        if self.movement_flag:
+            self.rotate_y(pg.time.get_ticks() % 0.005)
 
     def screen_projection(self):
         vertexes =  self.vertexes @ self.render.camera.camera_matrix() # перенос вершин объекта в пространство камеры
         vertexes =  vertexes @ self.render.projection.projection_matrix # перенос вершины в пространство отсечения
         vertexes /= vertexes[:,-1].reshape(-1,1) # нормализуем координаты вершин
-        vertexes[(vertexes > 1) | (vertexes < -1)] = 0
+        vertexes[(vertexes > 2) | (vertexes < -2)] = 0
         vertexes = vertexes @ self.render.projection.to_screen_matrix
         vertexes = vertexes[:, :2] # координаты для x,y,z
 
-        for face in self.faces: # проход по всем граням объекта для отображения нужной грани
+        for index, color_face in enumerate(self.color_faces): # проход по всем граням объекта для отображения нужной грани
+            color, face = color_face
             polygon = vertexes[face]
             if not np.any((polygon == self.render.H_WIDTH) | (polygon ==  self.render.H_HEIGHT)):
-                pg.draw.polygon(self.render.screen, pg.Color('orange'), polygon, 3)
+                pg.draw.polygon(self.render.screen, color, polygon, 3)
+                if self.label:
+                    text = self.font.render(self.label[index], True, pg.Color('white'))
+                    self.render.screen.blit(text, polygon[-1])
 
-        for vertex in vertexes: # отрисовка вершин объекта
-            if not np.any((vertex == self.render.H_WIDTH) | (vertex == self.render.H_HEIGHT)):
-                pg.draw.circle(self.render.screen, pg.Color('white'), vertex, 6)
+        if self.draw_vertexes: # разрешение строить вершины
+            for vertex in vertexes: # отрисовка вершин объекта
+                if not np.any((vertex == self.render.H_WIDTH) | (vertex == self.render.H_HEIGHT)):
+                    pg.draw.circle(self.render.screen, pg.Color('white'), vertex, 6)
 
 
 
@@ -45,3 +60,12 @@ class Object3D: #Класс для взимодействия с главной 
     def rotate_z(self,angle):
         self.vertexes = self.vertexes @ rotate_z(angle)
 
+class Axes(Object3D): # Класс осей
+    def __init__(self, render):
+        super().__init__(render)
+        self.vertexes = np.array([(0,0,0,1),(1,0,0,1),(0,1,0,1),(0,0,1,1)])
+        self.faces = np.array([(0,1),(0,2),(0,3)])
+        self.colors = [pg.Color('red'), pg.Color('green'),pg.Color('blue')]
+        self.color_faces = [(color, face) for color, face in zip(self.colors, self.faces)]
+        self.draw_vertexes = False
+        self.label = 'XYZ'
